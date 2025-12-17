67080694c24d52e00232ffde9342e0c8a92ff42f
Plrbala <103910505+Plrbala@users.noreply.github.com>
2025-12-17 10:55:37 +0530

    Refactor Mobile class with getters, setters, and constructor

--- DAY-2/Mobile.java
+++ DAY-2/Mobile.java
@@ -1,26 +1,57 @@
-package edu.mce.it.model;
-
 public class Mobile {
 
-    int mobileid;
-    String branchname;
-    String ownername;
-    double price;
 
-    
-    public Mobile(int mobileid, String branchname, String ownername, double price) {
+    private  int mobileid;
+    private String branchname;
+    private String ownername;
+    private double price;
+
+    public int getMobileid() {
+        return mobileid;
+    }
+
+    public void setMobileid(int mobileid) {
         this.mobileid = mobileid;
-        this.branchname = branchname;   
+    }
+
+    public String getBranchname() {
+        return branchname;
+    }
+
+    public void setBranchname(String branchname) {
+        this.branchname = branchname;
+    }
+
+    public String getOwnername() {
+        return ownername;
+    }
+
+    public void setOwnername(String ownername) {
         this.ownername = ownername;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 
-    @Override
-    public String toString() {
-        return String.format("%-10d%-15s%-15s%-15.2f",
-                this.mobileid,
-                this.branchname,
-                this.ownername,
-                this.price);
+    public Mobile(int mobileid, String branchname, String ownername, double price) {
+            this.mobileid = mobileid;
+            this.branchname = branchname;
+            this.ownername = ownername;
+            this.price = price;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%-10d%-15s%-15s%-15.2f",
+                    this.mobileid,
+                    this.branchname,
+                    this.ownername,
+                    this.price);
+        }
     }
-}
+
