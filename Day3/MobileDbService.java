a5897a997e349c7cec724210a561cce234b58c17
Plrbala <103910505+Plrbala@users.noreply.github.com>
2025-12-18 14:18:58 +0530

    Create MobileDbService.java

---
+++ Day3/MobileDbService.java
@@ -0,0 +1,86 @@
+package edu.mce.it.service;
+
+import edu.mce.it.DbUtil;
+import edu.mce.it.model.Mobile;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MobileDbService {
+    public MobileDbService()
+    {
+        createTable();
+    }
+    public void createTable()
+    {
+        String sql= """ 
+                CREATE TABLE IF NOT EXISTS mobile(
+                mobileid INT PRIMARY KEY,
+                brandname TEXT,
+                ownername TEXT,
+                price REAL)
+                """;
+        try(Connection con= DbUtil.getConnection();
+            Statement st=con.createStatement())
+        {
+            st.execute(sql);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public void addMobile(Mobile m)
+    {
+        String sql="INSERT INTO mobile VALUES(?,?,?,?)";
+        try(Connection con= DbUtil.getConnection();
+            PreparedStatement ps=con.prepareStatement(sql))
+        {
+            ps.setInt(1,m.getMobileid());
+            ps.setString(2,m.getBrandname());
+            ps.setString(3,m.getOwnername());
+            ps.setDouble(4,m.getPrice());
+            ps.executeUpdate();
+            System.out.println("mobile added successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void getMobiles()
+    {
+        String sql ="SELECT * FROM mobile";
+        try(Connection con= DbUtil.getConnection();
+            Statement st=con.createStatement();
+            ResultSet rs=st.executeQuery(sql))
+        {
+            System.out.println("\n ID  Brand  Owner  price");
+            while(rs.next())
+            {   System.out.printf("%-10d %-15s %-15s %.2f%n",
+                rs.getInt("mobileid"),
+                rs.getString("brandname"),
+                rs.getString("ownername"),
+                rs.getDouble("price"));
+            }
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
